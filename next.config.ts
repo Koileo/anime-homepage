498b733f7744338139f161d7f84f56a1d5299915
koileo <koileo@outlook.com>
2025-08-30 14:01:03 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+        pathname: '/**',   // 允许所有路径
+      },
+      {
+        protocol: 'https',
+        hostname: 'q.qlogo.cn',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ghchart.rshah.org',
+        pathname: '/**',
+      }
+    ],
+  },
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
